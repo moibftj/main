34ee752f63ee0e2e3f98c65d28562d70b4226e33
moizjamil <199167745+moibftj@users.noreply.github.com>
2025-11-24 00:47:16 +0000

    feat: Add animated letter type cards and update GenerateButton component styling

--- components/generate-button.tsx
+++ components/generate-button.tsx
@@ -24,44 +24,25 @@ export function GenerateButton({
       : 'Subscribe to Generate';
 
   return (
-    <div className={`btn-wrapper ${className}`}>
+    <div className={`continue-application-wrapper ${className}`}>
       <button
         type={type}
-        className="btn"
+        className="continue-application"
         onClick={onClick}
         disabled={loading || disabled}
       >
-        <svg
-          className="btn-svg"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
-          />
-        </svg>
-
-        <div className="txt-wrapper">
-          <div className="txt-1">
-            {buttonText.split('').map((letter, index) => (
-              <span key={index} className="btn-letter">
-                {letter}
-              </span>
-            ))}
-          </div>
-          <div className="txt-2">
-            {loading ? 'Generating...' : buttonText.split('').map((letter, index) => (
-              <span key={index} className="btn-letter">
-                {letter}
-              </span>
-            ))}
+        <div>
+          <div className="pencil"></div>
+          <div className="folder">
+            <div className="top">
+              <svg viewBox="0 0 24 27">
+                <path d="M1,0 L23,0 C23.5522847,-1.01453063e-16 24,0.44771525 24,1 L24,8.17157288 C24,8.70200585 23.7892863,9.21071368 23.4142136,9.58578644 L20.5857864,12.4142136 C20.2107137,12.7892863 20,13.2979941 20,13.8284271 L20,26 C20,26.5522847 19.5522847,27 19,27 L1,27 C0.44771525,27 6.76353751e-17,26.5522847 0,26 L0,1 C-6.76353751e-17,0.44771525 0.44771525,1.01453063e-16 1,0 Z"></path>
+              </svg>
+            </div>
+            <div className="paper"></div>
           </div>
         </div>
+        {buttonText}
       </button>
     </div>
   )
